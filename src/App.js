0dd5f3922b65b6d23081c7d93d761165e7c55c67
Bernardo Paes <bersantos22@gmail.com>
2022-02-09 22:13:09 -0300

    Semi-Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import { FoodBox } from './Components/FoodBox';
+ import foods from './foods.json' 
+import { AddNewFood } from './Components/AddNewFood';
+/* import {useState ,useEffect} from 'react'
+import axios from 'axios'; */
 
 function App() {
+  /* const [food, setFood] = useState([])
+
+    useEffect(()=>{
+        async function fetchFood(){
+            try{
+                const result = await axios.get('./foods.json');
+                setFood([...result.data])
+            } catch(error){
+                console.error(error)
+            }
+        }
+        fetchFood()
+    }) */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <h1 style={{fontSize:'35px',padding:'20px'}}>Nutritional Calculator</h1>
+    <AddNewFood />
+      <section>
+        {foods.map((currentFood)=>{
+          return (
+            <FoodBox
+              key={currentFood.name}
+              name={currentFood.name}
+              calories={currentFood.calories}
+              image={currentFood.image}
+            />
+          )
+        })}   
+      </section>
+
+    </>
   );
 }
 
---
+++ src/Components/AddNewFood.js
@@ -0,0 +1,52 @@
+import 'bulma/css/bulma.css';
+import React from 'react';
+import './addNewFood.css'
+import { useState } from 'react';
+export function AddNewFood(){
+
+ const [show,setShow] = useState(false)
+
+function handleButton(){
+    if (show=== false){
+        setShow(true)
+    }else{
+        setShow(false)
+    }
+}
+   
+    return(
+<div class='aqui' style={{padding:'20px'}}>
+
+    <div className="control">
+        <button onClick={handleButton} className="button is-link">Add Food Here </button>
+    </div>
+    <br></br>
+   {show? <section>
+            <div className="field">
+                <label className="label">Food name:</label>
+                <div className="control">
+                    <input className="input" name='name' type="text" placeholder=" Type the Food Name here"/>
+                </div>
+            </div>
+
+            <div className="field">
+                <label className="label">Calories:</label>
+                <div className="control">
+                    <input className="input" name='calories' type="text" placeholder="Type the number of calories"/>
+                </div>
+            </div>
+
+            <div className="field">
+                <label className="label">Image::</label>
+                <div className="control">
+                    <input className="input" name='image' type="text" placeholder="Type the url of the image"/>
+                </div>
+            </div>
+        <br></br>
+            <div className="control">
+        <button className="button is-primary">Submit</button>
+        </div>
+    </section> : null}
+ </div>   
+    )
+}
